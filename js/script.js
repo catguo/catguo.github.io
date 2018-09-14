29371f260a425815438b65913fa8dd7d7ec78e04
catherineguo <catguo88+github2@gmail.com>
2018-09-13 22:16:39 -0400

    Updated sections + smooth scrolling links in nav

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  $('nav a').on('click', function(event) {
+    event.preventDefault()
+    const hash = this.hash
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 400, function(){
+      window.location.hash = hash;
+    })
+  })
+})
