ce60a37f3fc540857b9656838e0a7fe4ac28c6cd
catherineguo <catguo88+github2@gmail.com>
2018-09-23 22:04:58 -0400

    Add scroll to top button

--- js/script.js
+++ js/script.js
@@ -5,7 +5,24 @@ $(document).ready(function () {
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 400, function () {
-      window.location.hash = hash;
+      window.location.hash = hash
     })
   })
+
+  const offset = 250
+  const duration = 300
+
+  $(window).scroll(function () {
+   if ($(this).scrollTop() > offset) {
+     $('.scroll-to-top').fadeIn(duration)
+   } else {
+     $('.scroll-to-top').fadeOut(duration)
+   }
+ })
+
+ $('.scroll-to-top').on('click', function (event) {
+   event.preventDefault()
+   $('html, body').animate({scrollTop: 0}, duration)
+   return false
+ })
 })
